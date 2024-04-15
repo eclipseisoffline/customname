be370d580c327bb756d5e01acdaa130b34ab78a6
Eclipse <eclipse@eclipseisoffline.xyz>
2024-04-15 16:22:11 +0000

    Add recursion check

--- src/main/java/xyz/eclipseisoffline/eclipsescustomname/mixin/PlayerEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsescustomname/mixin/PlayerEntityMixin.java
@@ -1,15 +1,23 @@
 package xyz.eclipseisoffline.eclipsescustomname.mixin;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.eclipseisoffline.eclipsescustomname.PlayerNameManager;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin extends LivingEntity {
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType,
+            World world) {
+        super(entityType, world);
+    }
 
     @Redirect(method = "getDisplayName", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getName()Lnet/minecraft/text/Text;"))
     public Text setCustomName(PlayerEntity player) {
