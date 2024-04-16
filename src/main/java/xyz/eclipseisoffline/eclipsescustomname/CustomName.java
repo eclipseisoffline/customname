a57070d5b02bac817e96abb12f1b2440e79645a8
Eclipse <eclipse@eclipseisoffline.xyz>
2024-04-16 08:08:05 +0000

    Fix formatting not applying correctly with hex codes

--- src/main/java/xyz/eclipseisoffline/eclipsescustomname/CustomName.java
+++ src/main/java/xyz/eclipseisoffline/eclipsescustomname/CustomName.java
@@ -18,6 +18,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
@@ -93,9 +94,14 @@ public class CustomName implements ModInitializer {
                                                             Text.of("Must hold an item to name")).create();
                                                 }
 
-                                                Text argument = argumentToText(
-                                                        StringArgumentType.getString(context,
-                                                                "name"), true, true, true);
+                                                Text argument;
+                                                try {
+                                                    argument = argumentToText(
+                                                            StringArgumentType.getString(context,
+                                                                    "name"), true, true, true);
+                                                } catch (IllegalArgumentException exception) {
+                                                    throw new SimpleCommandExceptionType(Text.of(exception.getMessage())).create();
+                                                }
                                                 if (Formatting.strip(argument.getString()).isEmpty()) {
                                                     throw new SimpleCommandExceptionType(
                                                             Text.of("Invalid item name")).create();
@@ -186,13 +192,13 @@ public class CustomName implements ModInitializer {
         }
         if (formattingEnabled) {
             MutableText complete = Text.empty();
-            MutableText currentText = Text.empty();
-
-            if (forceItalics) {
-                currentText = currentText.styled(style -> style.withItalic(false));
-            }
 
             StringReader argumentReader = new StringReader(argument);
+            StringBuilder currentText = new StringBuilder();
+            Style currentStyle = Style.EMPTY;
+            if (forceItalics) {
+                currentStyle = currentStyle.withItalic(false);
+            }
 
             try {
                 int c = argumentReader.read();
@@ -208,10 +214,14 @@ public class CustomName implements ModInitializer {
                         formatting = true;
                     } else if (current == HEX_CODE && formatting) {
                         formatting = false;
-                        complete.append(currentText);
-                        currentText = Text.empty();
+                        if (!currentText.isEmpty()) {
+                            complete.append(Text.literal(currentText.toString()).setStyle(currentStyle));
+                        }
+
+                        currentText = new StringBuilder();
+                        currentStyle = Style.EMPTY;
                         if (forceItalics) {
-                            currentText = currentText.styled(style -> style.withItalic(false));
+                            currentStyle = currentStyle.withItalic(false);
                         }
 
                         char[] hexChars = new char[6];
@@ -222,11 +232,13 @@ public class CustomName implements ModInitializer {
 
                         try {
                             int colour = Integer.parseInt(String.valueOf(hexChars), 16);
-                            currentText.styled(style -> style.withColor(colour));
+                            currentStyle = currentStyle.withColor(colour);
                         } catch (NumberFormatException exception) {
                             throw new IllegalArgumentException("Invalid hex formatting code",
                                     exception);
                         }
+
+                        wasFormatting = true;
                     } else if (formatting) {
                         formatting = false;
 
@@ -234,18 +246,23 @@ public class CustomName implements ModInitializer {
                         if (newStyle == null) {
                             throw new IllegalArgumentException("Invalid formatting code");
                         }
+
                         if (newStyle.isColor() || newStyle == Formatting.RESET || !wasFormatting) {
-                            complete.append(currentText);
-                            currentText = Text.empty();
+                            if (!currentText.isEmpty()) {
+                                complete.append(Text.literal(currentText.toString()).setStyle(currentStyle));
+                            }
+
+                            currentText = new StringBuilder();
+                            currentStyle = Style.EMPTY;
                             if (forceItalics) {
-                                currentText = currentText.styled(style -> style.withItalic(false));
+                                currentStyle = currentStyle.withItalic(false);
                             }
                         }
                         wasFormatting = true;
-                        currentText.styled(style -> style.withFormatting(newStyle));
+                        currentStyle = currentStyle.withFormatting(newStyle);
                     } else {
                         wasFormatting = false;
-                        currentText.append(String.valueOf(current));
+                        currentText.append(current);
                     }
 
                     c = argumentReader.read();
@@ -254,7 +271,9 @@ public class CustomName implements ModInitializer {
                 throw new AssertionError(exception);
             }
 
-            complete.append(currentText);
+            if (!currentText.isEmpty()) {
+                complete.append(Text.literal(currentText.toString()).setStyle(currentStyle));
+            }
             return complete;
         }
         return Text.of(argument);
