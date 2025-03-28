491f8b932e06cbf5d06380d40ce97dfaf5e81d26
Eclipse <eclipse@eclipseisoffline.xyz>
2025-03-28 08:53:52 +0000

    Fix a bug, improve compatibility with other mods

--- src/main/java/xyz/eclipseisoffline/eclipsescustomname/mixin/entity/PlayerEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/eclipsescustomname/mixin/entity/PlayerEntityMixin.java
@@ -1,5 +1,7 @@
 package xyz.eclipseisoffline.eclipsescustomname.mixin.entity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,7 +10,6 @@ import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.eclipseisoffline.eclipsescustomname.CustomName;
 import xyz.eclipseisoffline.eclipsescustomname.PlayerNameManager;
 
@@ -20,11 +21,11 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         super(entityType, world);
     }
 
-    @Redirect(method = "getDisplayName", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getName()Lnet/minecraft/text/Text;")) // TODO wrapop
-    public Text setCustomName(PlayerEntity player) {
+    @WrapOperation(method = "getDisplayName", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getName()Lnet/minecraft/text/Text;"))
+    public Text setCustomName(PlayerEntity player, Operation<Text> original) {
         if (player instanceof ServerPlayerEntity serverPlayer) {
             return PlayerNameManager.getPlayerNameManager(serverPlayer.server, CustomName.getConfig()).getFullPlayerName(serverPlayer);
         }
-        return player.getName();
+        return original.call(player);
     }
 }
