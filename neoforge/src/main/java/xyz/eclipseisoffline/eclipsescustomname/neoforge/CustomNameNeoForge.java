58b1f959a7aee58b83cc45a56e02c0096c6d119c
Eclipse <eclipse@eclipseisoffline.xyz>
2026-01-08 10:31:03 +0000

    Start work on neoforge port

---
+++ neoforge/src/main/java/xyz/eclipseisoffline/eclipsescustomname/neoforge/CustomNameNeoForge.java
@@ -0,0 +1,45 @@
+package xyz.eclipseisoffline.eclipsescustomname.neoforge;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import xyz.eclipseisoffline.eclipsescustomname.CustomName;
+import xyz.eclipseisoffline.eclipsescustomname.CustomNamePermissions;
+
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+@Mod(CustomName.MOD_ID)
+public class CustomNameNeoForge extends CustomName {
+    private final String version;
+
+    public CustomNameNeoForge(ModContainer mod) {
+        version = mod.getModInfo().getVersion().getQualifier();
+
+        initialize();
+    }
+
+    @Override
+    protected String getVersion() {
+        return version;
+    }
+
+    @Override
+    protected Path getConfigDir() {
+        return FMLPaths.CONFIGDIR.get();
+    }
+
+    @Override
+    protected CustomNamePermissions createPermissions() {
+        return (_,  _) -> false; // TODO
+    }
+
+    @Override
+    protected void registerCommands(Consumer<CommandDispatcher<CommandSourceStack>> registerer) {
+        NeoForge.EVENT_BUS.addListener(RegisterCommandsEvent.class, event -> registerer.accept(event.getDispatcher()));
+    }
+}
