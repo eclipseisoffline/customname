6bfb90c227d8ad7f67b2d56c455ce43639a2b0df
Eclipse <eclipse@eclipseisoffline.xyz>
2026-03-15 20:24:40 +0000

    Null-mark new packages

---
+++ common/src/main/java/xyz/eclipseisoffline/eclipsescustomname/filefix/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package xyz.eclipseisoffline.eclipsescustomname.filefix;
+
+import org.jspecify.annotations.NullMarked;
---
+++ common/src/main/java/xyz/eclipseisoffline/eclipsescustomname/mixin/filefix/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package xyz.eclipseisoffline.eclipsescustomname.mixin.filefix;
+
+import org.jspecify.annotations.NullMarked;
